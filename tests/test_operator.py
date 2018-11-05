95c300da47d376742ce85b56ac938bbd70fe1659
boleideng1994 <boleideng@g.harvard.edu>
2018-11-04 21:11:01 -0500

    test_div added

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -35,3 +35,16 @@ class Test_Operator():
         x3 = 3*x1*x2*3
         assert x3.val == 36
         assert x3.der == {'x1': 9, 'x2': 36}
+
+    def test_div(self):
+        x1 = Variable(1, name='x1')
+        x2 = Variable(4, name='x2')
+        x3 = x1/x2
+        x4 = x1/3
+        x5 = 3/x1
+        assert x3.val == 1/4
+        assert x3.der == {'x1': 1/4, 'x2': -1/16}
+        assert x4.val == 1/3
+        assert x4.der == {'x1': 1/3}  
+        assert x5.val == 3
+        assert x5.der == {'x1': -3}
