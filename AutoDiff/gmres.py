c81c036a2ccc7b0ab5e68c8cc3c9eab83306dfdb
YANYING QIU <yqiu@vpn-6-35.vpnclient.harvard.edu>
2018-12-07 13:37:23 -0500

    solve global var issue

--- AutoDiff/gmres.py
+++ AutoDiff/gmres.py
@@ -1,12 +1,9 @@
 from AutoDiff.AutoDiff import Variable
 import numpy as np
 from scipy.sparse.linalg import gmres, LinearOperator
+from demos.GLOBAL_VARS import F, b
 
-A = np.array([[2, 3, 2], 
-               [3, 2, 1],
-               [3, 3, 3]])
-b = np.array([1, 2, 3])
-    
+
 def grad(p):
     """Calculates derivatives of Ax where A is a matrix using forward mode automatic differentiation
     
@@ -20,24 +17,17 @@ def grad(p):
     np.array
     dot product of J and p
     """
-    x = [Variable(1, name='x1', der=p)] * A.shape[0]
-    f = [0] * A.shape[0]
-    J = np.zeros(A.shape)
-    for i in range(A.shape[0]):
-        x[i] = Variable(1, name='x'+str(i), der=p)
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            f[i] += A[i, j]*x[j]
-        J[i] = [v[0] for k, v in sorted(f[i].der.items())]
+    dim = len(F)
+    J = np.zeros((dim, dim))
+    for i in range(dim):
+        J[i] = [v[0] for k, v in sorted(F[i].der.items())]
     return np.dot(J, p)
 
-def gmres_autodiff(A, b, grad):
+def gmres_autodiff(b, grad):
     """Solves Ax=b using GMRes with automatic differentiation
     
     Parameters
     ==========
-    A:     np.array (1d)
-           A in Ax=b
     b:     np.array (1d)
            RHS of Ax=b
     grad:  function
@@ -55,6 +45,7 @@ def gmres_autodiff(A, b, grad):
     >>> x
     array([ 1., -1.,  1.])
     """
-    action = LinearOperator((b.shape[0], b.shape[0]), matvec=grad)
+    dim = b.shape[0]
+    action = LinearOperator((dim, dim), matvec=grad)
     x, exitcode = gmres(action, b)
     return x
