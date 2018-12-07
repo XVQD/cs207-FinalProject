dbabbca2b252e27316041b925f1e22d397b4027e
YANYING QIU <yqiu@vpn-6-35.vpnclient.harvard.edu>
2018-12-06 23:37:48 -0500

    uncomment out __str__

--- AutoDiff/AutoDiff.py
+++ AutoDiff/AutoDiff.py
@@ -35,7 +35,7 @@ class Variable:
     >>> from AutoDiff import Variable
     >>> x1 = Variable(1, name='x1') # register independent variables by specifying names
     >>> x2 = x1 + 1
-    >>> x3 = Variable(7, 'x3')
+    >>> x3 = Variable(7, name='x3')
     >>> x4 =  x2 * x3
     >>> print(x4.val, x4.der)  # only calculate derivatives of registered(i.e.named) independent variables 
     14 {'x1': 7, 'x3': 2} 
@@ -53,15 +53,17 @@ class Variable:
         # if a name is supplied, then create a new variable with its own derivative
         if name!= None:
             self.der = {name: np.ones(lenn)} # the first derivative of a variable is 1
-            self.der2 = {name+name: np.zeros(lenn)} # the first derivative of a variable is 0
+            self.der2 = {name+name: np.zeros(lenn)} # the second derivative of a variable is 0
         else:
             self.der = der
             self.der2 = der2
+
     def __str__(self):
-#         if self.name==None:
-#             return "ad.Variable(val={})".format(self.val )
-#         else:
-        return "ad.Variable(val={},\n name='{}', \n der={}, \n der2={})".format(self.val,self.name,self.der,self.der2 )
+        if self.name==None:
+            return "ad.Variable(val={})".format(self.val )
+        else:
+            return "ad.Variable(val={},\n name='{}', \n der={}, \n der2= {})".format(self.val,self.name,self.der,self.der2 )
+
     def __pos__(self):
         """Returns the Variable itself. Does nothing to value or derivative."""
         return Variable(self.val, der=self.der, der2=self.der2)
@@ -87,7 +89,7 @@ class Variable:
             der={x: a.get(x, 0) + b.get(x, 0) for x in set(a).union(b)}
             a2,b2 = self._expand(a,b,a2,b2)
             der2={x: a2.get(x, 0) + b2.get(x, 0) for x in set(a2).union(b2)}
-            print(der2)
+            #print(der2)
             return Variable(self.val+other.val, der=der, der2 = der2)
         # other is not a Variable
         except AttributeError:
