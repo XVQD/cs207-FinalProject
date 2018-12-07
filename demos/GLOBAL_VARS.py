bf804db47456442f24a5f730a7b0045d36d1938b
YANYING QIU <yqiu@vpn-6-35.vpnclient.harvard.edu>
2018-12-07 13:38:40 -0500

    put global vars in one file

---
+++ demos/GLOBAL_VARS.py
@@ -0,0 +1,11 @@
+from AutoDiff.AutoDiff import Variable
+import numpy as np
+
+x1 = Variable(1, name='x1')
+x2 = Variable(1, name='x2')
+x3 = Variable(1, name='x3')
+f1 = 2*x1+3*x2+2*x3
+f2 = 3*x1+2*x2+1*x3
+f3 = 3*x1+3*x2+3*x3
+F = [f1, f2, f3]
+b = np.array([1, 2, 3])
