cc9cf37b300903435bfcc127ee6e8e6df35fdbeb
YANYING QIU <yqiu@YANYINGs-MacBook-Pro.local>
2018-12-09 15:02:43 -0500

    accommodate new closure function

--- demos/gmres_demo.py
+++ demos/gmres_demo.py
@@ -1,5 +1,5 @@
 from AutoDiff.AutoDiff import Variable
-from AutoDiff.gmres import grad, gmres_autodiff
+from AutoDiff.gmres import autograd, gmres_autodiff
 import numpy as np
 from scipy.sparse.linalg import gmres, LinearOperator
 import sympy as sym
@@ -15,38 +15,12 @@ f3 = 3*x1+3*x2+3*x3
 F = [f1, f2, f3]
 b = np.array([1, 2, 3])
 
-# define action on x
+# define action
+grad = autograd(F)
+
+# define LinearOperator
 action = LinearOperator((3, 3), matvec=grad)
 
 # GMRES to get x
 x, exitcode = gmres(action, b)
 print(x)
-
-# function to construct Jacobian matrix
-def Jacobian(v_str, f_list):
-    vars = sym.symbols(v_str)
-    f = sym.sympify(f_list)
-    J = sym.zeros(len(f),len(vars))
-    for i, fi in enumerate(f):
-        for j, s in enumerate(vars):
-            J[i,j] = sym.diff(fi, s)
-    return J
-
-# time the Jacobian matrix construction for matrices of different sizes
-times_jacobian = []
-for size_n in range(10, 110, 10):
-    var = ' '.join([''.join(x) for x in list(zip(['x']*size_n, 
-                   [str(x) for x in list(range(1, size_n+1, 1))]))])
-    coefs = np.random.rand(size_n, size_n)
-    f = [''] * size_n
-    for i in range(coefs.shape[0]):
-        for j in range(coefs.shape[1]):
-            f[i] += str(coefs[i, j])+'*'+var.split(' ')[j]
-            if j != coefs.shape[0]-1:
-                f[i] += '+'
-    start = time.time()
-    Jacobian(var, f)
-    end = time.time()
-    times_jacobian.append(end-start)
-
-print('Jacobian time', times_jacobian)
