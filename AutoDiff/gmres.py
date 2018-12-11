9842614f128a1cb2ee0b470e7d81a6fe22b28778
YANYING QIU <yqiu@YANYINGs-MacBook-Pro.local>
2018-12-11 11:34:18 -0500

    change format of gmres function

--- AutoDiff/gmres.py
+++ AutoDiff/gmres.py
@@ -1,21 +1,23 @@
-from AutoDiff.AutoDiff import Variable
+import AutoDiff.AutoDiff as ad
 import numpy as np
 from scipy.sparse.linalg import gmres, LinearOperator
 
 def autograd(F):
+    """Calculates derivatives of Ax where A is a matrix using forward mode automatic differentiation
+
+    Parameters
+    ==========
+    F:  list
+        user-defined vector of vector functions
+    p:  np.array
+        the vector p in Jp matrix-vector product where J is the Jacobian
+
+    Returns
+    =======
+    function
+    function to get matrix-vector product
+    """
     def grad(p):
-        """Calculates derivatives of Ax where A is a matrix using forward mode automatic differentiation
-
-        Parameters
-        ==========
-        p:  np.array
-            the vector p in Jp matrix-vector product where J is the Jacobian
-
-        Returns
-        =======
-        np.array
-        dot product of J and p
-        """
         dim = len(F)
         J = np.zeros((dim, dim))
         for i in range(dim):
@@ -23,15 +25,15 @@ def autograd(F):
         return np.dot(J, p)
     return grad
 
-def gmres_autodiff(b, grad):
+def gmres_autodiff(F, b):
     """Solves Ax=b using GMRes with automatic differentiation
     
     Parameters
     ==========
-    b:     np.array (1d)
+    F:     list
+           vector of vector functions
+    b:     list
            RHS of Ax=b
-    grad:  function
-           function to calculate dot product of J and p
          
     Returns
     =======
@@ -40,12 +42,20 @@ def gmres_autodiff(b, grad):
     
     Examples
     ========
-    >>> b = np.array([2, 3, 1])
-    >>> x, exitcode = gmres_autodiff(b)
+    >>> b = [2, 3, 1]
+    >>> x1 = ad.Variable(1, name='x1')
+    >>> x2 = ad.Variable(2, name='x2')
+    >>> x3 = ad.Variable(2, name='x3')
+    >>> f1 = 2*x1+3*x2+2*x3
+    >>> f2 = 3*x1+2*x2+1*x3
+    >>> f3 = 3*x1+3*x2+3*x3
+    >>> F = [f1, f2, f3]
+    >>> x = gmres_autodiff(F, b)
     >>> x
     array([ 1., -1.,  1.])
     """
-    dim = b.shape[0]
+    dim = np.array(b).shape[0]
+    grad = autograd(F)
     action = LinearOperator((dim, dim), matvec=grad)
     x, exitcode = gmres(action, b, atol='legacy')
     return x
