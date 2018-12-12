09a3b2dbdc7970d3337a9929d3693ae8f2a9d134
YANYING QIU <yqiu@YANYINGs-MacBook-Pro.local>
2018-12-11 23:09:11 -0500

    add test for 2nd der of sigmoid

--- tests/test_second_derivatives.py
+++ tests/test_second_derivatives.py
@@ -114,3 +114,8 @@ class Test_second_derivatives():
         x1 = Variable(1, name='x1')
         z1 = AutoDiff.sqrt(x1)
         assert z1.der2['x1x1'][0] == -0.25
+
+    def test_sigmoid(self):
+        x1 = Variable(0, name='x1')
+        z1 = AutoDiff.sigmoid(x1)
+        assert z1.der2['x1x1'][0] == 0
